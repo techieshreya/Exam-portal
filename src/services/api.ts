f0057eb3e55e8c7a334e63dd4e4bf8bc50856021
techieshreya <shreyagupta.ggn@gmail.com>
2025-04-05 15:15:37 +0530

    fix: update backend URL for API integration

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: "https://unisphere-api.clusterider.tech/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
